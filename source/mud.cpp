80020797ad447941d962dbf2d26df38a6a76f258
bilalch <bilal@tintash.com>
2013-09-22 23:13:36 +0500

    collision with mud fixed

--- source/mud.cpp
+++ source/mud.cpp
@@ -6,7 +6,7 @@ int Mud::update(PlayerCar *m_char, ScrollingBackground *m_scroll_bg)
 	m_x2-=m_scroll_bg->getScrollSpeed();
 
 	//detect collision
-	if (m_char->getY2() > m_y1 - 100)
+	if (m_char->getY2() > m_y1)
 	{
 		if ((m_char->getX1() > m_x1 && m_char->getX1() < m_x2) || 
 			(m_char->getX2() > m_x1 && m_char->getX2() < m_x2) ||
