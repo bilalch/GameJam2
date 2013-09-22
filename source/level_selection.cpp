fa7682b614a4ed1243220796d6ab3ed778720058
bilalch <bilal@tintash.com>
2013-09-23 02:30:19 +0500

    creating story

---
+++ source/level_selection.cpp
@@ -0,0 +1,21 @@
+#include "level_selection.h"
+
+LevelSelection::LevelSelection()
+{
+
+}
+
+LevelSelection::~LevelSelection()
+{
+
+}
+
+int LevelSelection::update()
+{
+
+}
+
+void LevelSelection::draw()
+{
+
+}
---
+++ source/level_selection.h
@@ -0,0 +1,19 @@
+#ifndef LEVEL_SELECTION_H
+#define LEVEL_SELECTION_H
+
+#include "observer.h"
+
+class LevelSelection
+{
+public:
+	LevelSelection();
+	~LevelSelection();
+
+	int update();
+	void draw();
+
+private:
+	
+};
+
+#endif //LEVEL_SELECTION_H
